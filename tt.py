bc0fab67ab1593d1b7bb6058b4f98bd2f7302910
Arjun Kavungal <47582621+yimen3@users.noreply.github.com>
2022-06-23 21:07:55 -0400

    Update tt.py

--- tt.py
+++ tt.py
@@ -4,7 +4,6 @@ from sklearn.linear_model import LinearRegression
 X = [[1, 1], [1, 2], [2, 2], [2, 3]]
 y = [0,1,2,3]
 reg = LinearRegression().fit(X, y)
-reg.predict(np.array([[3, 5]]))
 title = st.text_input('Movie title', '')
 title2 = st.text_input('Movie title', '')
 reg.predict([[int(title), int(title2)]])
