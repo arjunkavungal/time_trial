e31019ca1a70d58e6be73f91ca1894c27e470560
Arjun Kavungal <arjunkavungal@gmail.com>
2023-03-07 11:25:44 -0500

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-X = [[1, 1], [1, 2], [2, 2], [2, 3]]
-y = [0,1,2,3]
+X = [[1, 1], [1, 2], [2, 2], [2, 3],[3,4]]
+y = [0,1,2,3,4]
 reg = LinearRegression().fit(X, y)
 title = st.text_input('Movie title', '')
 title2 = st.text_input('a', '')
